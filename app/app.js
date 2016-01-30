83e20569d59da4c71598a448130f682ba5e3b75f
mastermoin <mastermoin409@gmail.com>
2016-01-30 12:42:47 -0500

    first commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ui.router',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/view1");
+});
